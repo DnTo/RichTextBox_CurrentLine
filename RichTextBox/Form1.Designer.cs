bff7015c05869d03d7810cb715ed0175d82fd3c8
danto <danto@GOBOX>
2021-03-17 04:27:40 -0500

    Add project files.

---
+++ RichTextBox/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace RichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbNumberOfLines = new System.Windows.Forms.TextBox();
+            this.tbEndOfLinesIndex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSelectedText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCurrentLine = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbNumberOfLines
+            // 
+            this.tbNumberOfLines.Location = new System.Drawing.Point(343, 27);
+            this.tbNumberOfLines.Name = "tbNumberOfLines";
+            this.tbNumberOfLines.Size = new System.Drawing.Size(100, 22);
+            this.tbNumberOfLines.TabIndex = 1;
+            // 
+            // tbEndOfLinesIndex
+            // 
+            this.tbEndOfLinesIndex.Location = new System.Drawing.Point(343, 61);
+            this.tbEndOfLinesIndex.Name = "tbEndOfLinesIndex";
+            this.tbEndOfLinesIndex.Size = new System.Drawing.Size(100, 22);
+            this.tbEndOfLinesIndex.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "#OfLines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Index EndOfLines";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "selectedText";
+            // 
+            // tbSelectedText
+            // 
+            this.tbSelectedText.Location = new System.Drawing.Point(111, 267);
+            this.tbSelectedText.Name = "tbSelectedText";
+            this.tbSelectedText.Size = new System.Drawing.Size(100, 22);
+            this.tbSelectedText.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "currentLine";
+            // 
+            // tbCurrentLine
+            // 
+            this.tbCurrentLine.Location = new System.Drawing.Point(111, 300);
+            this.tbCurrentLine.Name = "tbCurrentLine";
+            this.tbCurrentLine.Size = new System.Drawing.Size(100, 22);
+            this.tbCurrentLine.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(191, 249);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(RichTextBox.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 342);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbCurrentLine);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbSelectedText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbEndOfLinesIndex);
+            this.Controls.Add(this.tbNumberOfLines);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumberOfLines;
+        private System.Windows.Forms.TextBox tbEndOfLinesIndex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSelectedText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbCurrentLine;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+    }
+}
+
---
+++ RichTextBox/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            var rtb = sender as System.Windows.Forms.RichTextBox;
+           
+            this.tbNumberOfLines.Text = rtb.Lines.Count().ToString();
+            this.tbEndOfLinesIndex.Text = string.Join("-", rtb.Lines.ToList().Select(x => x.Length));
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            var rtb = sender as System.Windows.Forms.RichTextBox;
+            tbSelectedText.Text = rtb.SelectedText;
+
+            int cursorPosition = rtb.SelectionStart;
+            var index = rtb.GetLineFromCharIndex(cursorPosition);
+            tbCurrentLine.Text = index.ToString();
+        }
+    }
+}
